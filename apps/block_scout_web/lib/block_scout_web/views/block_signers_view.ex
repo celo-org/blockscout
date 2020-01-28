4729baae5f7b6bfb1002a416b77bd668997cd86e
Sami Mäkelä <mrsmkl@gmail.com>
2020-01-28 13:15:22 +0200

    fixed now?

--- apps/block_scout_web/lib/block_scout_web/views/block_signers_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/block_signers_view.ex
@@ -10,8 +10,8 @@ defmodule BlockScoutWeb.BlockSignersView do
          Ecto.assoc_loaded?(member.celo_delegator) and
          member.celo_delegator != nil and
          member.celo_delegator.celo_account != nil and
-         Ecto.assoc_loaded?(member.celo_delegator.celo_account) do
-      IO.inspect(member.celo_delegator.celo_account)
+         Ecto.assoc_loaded?(member.celo_delegator.celo_account) and 
+         member.celo_delegator.celo_account.name != nil do
       named = %Address.Name{
         address: member.celo_delegator.celo_account.account_address,
         address_hash: member.celo_delegator.celo_account.address,
