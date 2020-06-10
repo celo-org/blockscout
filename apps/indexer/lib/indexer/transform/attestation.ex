8efcaef07393a540075a457e7b825b916dd16efa
Sami Mäkelä <mrsmkl@gmail.com>
2020-06-10 18:14:12 +0300

    fixed it so that it compiles

--- apps/indexer/lib/indexer/transform/attestation.ex
+++ apps/indexer/lib/indexer/transform/attestation.ex
@@ -16,20 +16,20 @@ defmodule Indexer.Transform.Attestation do
   defp parse_params(%{first_topic: first_topic, second_topic: second_topic, third_topic: third_topic, fourth_topic: fourth_topic, block_number: block_number})
        when not is_nil(second_topic) and not is_nil(third_topic) and not is_nil(fourth_topic) do
     %{
-      attestor: truncate_address_hash(fourth_topic),
-      attestee: truncate_address_hash(third_topic),
+      attestor_hash: truncate_address_hash(fourth_topic),
+      attestee_hash: truncate_address_hash(third_topic),
       identifier: second_topic,
       status: status_for(first_topic),
       block_number: block_number
     }
   end
 
-  defp status_for(first_topic) when first_topic == CeloAccount.attestation_completed_event do
-    "completed"
-  end
-
-  defp status_for(first_topic) when first_topic == CeloAccount.attestation_completed_event do
-    "requested"
+  defp status_for(first_topic) do
+    if first_topic == CeloAccount.attestation_completed_event do
+      "completed"
+    else
+      "requested"
+    end
   end
 
   defp truncate_address_hash("0x000000000000000000000000" <> truncated_hash) do
