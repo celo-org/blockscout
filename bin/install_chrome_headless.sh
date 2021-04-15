5687a184f973934fe9ba9601b24fa37905e5095a
Aaron Boyd <aaronmboyd@gmail.com>
2021-04-15 14:48:35 +0200

    [CHORE] - Downgrade CHROMEDRIVER_VERSION for debugging

--- bin/install_chrome_headless.sh
+++ bin/install_chrome_headless.sh
@@ -1,6 +1,7 @@
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
-export CHROMEDRIVER_VERSION=`curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE`
+# export CHROMEDRIVER_VERSION=`curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE`
+export CHROMEDRIVER_VERSION="88.0.4324.96"
 curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip
 sudo chmod +x chromedriver
