4bc51a0901d67bbe84b5dc6c2bad12b872579938
obasilakis <vasileios.georgopoulos@gmail.com>
2021-06-26 06:16:19 +0300

    Expose web metrics under /metrics/web

--- apps/block_scout_web/config/config.exs
+++ apps/block_scout_web/config/config.exs
@@ -156,3 +156,6 @@ config :block_scout_web, BlockScoutWeb.WebRouter, enabled: System.get_env("DISAB
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :prometheus, BlockScoutWeb.Prometheus.Exporter,
+       path: "/metrics/web"
