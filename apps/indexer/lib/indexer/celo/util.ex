86b92e4eb4eface2256cec1b7d78adcab1f18a7b
Donald Hutchison <donald.hutchison@clabs.co>
2021-09-02 16:16:42 +0200

    Credo poke.

--- apps/indexer/lib/indexer/celo/util.ex
+++ apps/indexer/lib/indexer/celo/util.ex
@@ -1,20 +1,23 @@
+# credo:disable-for-this-file
 defmodule Indexer.Celo.Util do
+  @moduledoc "Runtime helper methods for concurrency tuning"
+
   def set_internal_transaction_batch_size(size) do
-    internal_transaction_fetcher_pid
+    internal_transaction_fetcher_pid()
     |> :sys.replace_state(fn state ->
       %{state | max_batch_size: size}
     end)
   end
 
   def set_internal_transaction_concurrency(size) do
-    internal_transaction_fetcher_pid
+    internal_transaction_fetcher_pid()
     |> :sys.replace_state(fn state ->
       %{state | max_concurrency: size}
     end)
   end
 
   def get_internal_transaction_state() do
-    internal_transaction_fetcher_pid
+    internal_transaction_fetcher_pid()
     |> :sys.get_state()
   end
 
