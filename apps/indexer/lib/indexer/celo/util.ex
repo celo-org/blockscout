30caf111248e6e6844bb12fb66d0105d0d72a4dd
Donald Hutchison <donald.hutchison@clabs.co>
2021-09-02 15:56:59 +0200

    Formatting.

---
+++ apps/indexer/lib/indexer/celo/util.ex
@@ -0,0 +1,29 @@
+defmodule Indexer.Celo.Util do
+  def set_internal_transaction_batch_size(size) do
+    internal_transaction_fetcher_pid
+    |> :sys.replace_state(fn state ->
+      %{state | max_batch_size: size}
+    end)
+  end
+
+  def set_internal_transaction_concurrency(size) do
+    internal_transaction_fetcher_pid
+    |> :sys.replace_state(fn state ->
+      %{state | max_concurrency: size}
+    end)
+  end
+
+  def get_internal_transaction_state() do
+     internal_transaction_fetcher_pid
+     |> :sys.get_state
+  end
+
+  defp internal_transaction_fetcher_pid do
+    {_child_id, pid, _, _} =
+      Supervisor.which_children(Indexer.Fetcher.InternalTransaction.Supervisor)
+      |> Enum.find(fn {Indexer.Fetcher.InternalTransaction,_, _,_} -> true
+        _ -> false
+      end)
+    pid
+  end
+end
