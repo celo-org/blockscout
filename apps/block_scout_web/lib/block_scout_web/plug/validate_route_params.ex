adb8e471fb0c0f24969d9312e41465eb9cb7777c
Donald Hutchison <donald.hutchison@clabs.co>
2021-09-21 18:40:10 +0200

    Redirect to validation error page when failed.

--- apps/block_scout_web/lib/block_scout_web/plug/validate_route_params.ex
+++ apps/block_scout_web/lib/block_scout_web/plug/validate_route_params.ex
@@ -6,13 +6,12 @@ defmodule BlockScoutWeb.ValidateRouteParams do
   the private field of the Plug.Conn object. This plug is designed to fail safe, that is - unless a parameter has
   been found to be explicitly invalid it will be treated as invalid.
 
-  Validation functions can be any function that returns a boolean variable, an atom representing any of the kernel
-  functions (e.g. :is_integer, :is_float) or :is_address which invokes Explorer.Chain.Hash.Address.validate/1.
+  Validation functions can be any function that returns a boolean variable or :is_address which invokes
+  Explorer.Chain.Hash.Address.validate/1.
   """
 
-  import Plug.Conn
+  import BlockScoutWeb.Controller, only: [validation_failed: 1]
 
-  import Phoenix.Controller, only: [not_found: 1]
   alias Explorer.Chain.Hash.Address
 
   def init(opts), do: opts
@@ -20,34 +19,33 @@ defmodule BlockScoutWeb.ValidateRouteParams do
   def call(conn = %{params: params, private: %{validate: validation}}, _) do
     validate(conn, params, validation)
   end
+  def call(conn, _), do: conn
 
-  def validate(conn, %{}, _validation), do: conn
   def validate(conn, params, validation) when is_map(validation) do
     invalid =
       validation
-      |> Enum.reduce( fn {param, validate_func} ->
+      |> Enum.map( fn {param, validate_func} ->
        perform_validation(params[param], validate_func)
       end)
       |> Enum.any?(fn valid -> valid == false end)
 
    if invalid do
      conn
-     |> not_found()
-     |> halt()
+     |> validation_failed()
    else
     conn
    end
   end
+  def validate(conn, %{}, _validation), do: conn
   def validate(conn, _, _), do: conn
 
-  def perform_validation(p, validator) when is_function(validator), do: validator(p)
-  def perform_validation(p, validator) when is_atom(validator), do: apply(__MODULE__, validator, [1, p])
-  def perform_validation(nil, validator), do: true
-
-  def call(conn, _) do
-    IO.inspect(conn)
-    conn
+  def perform_validation(p, validator) when is_function(validator), do: validator.(p)
+  def perform_validation(p, validator) when is_atom(validator) do
+    case validator do
+      :is_address -> perform_validation(p, &is_address/1)
+    end
   end
+  def perform_validation(nil, _validator), do: true
 
   defp is_address(param) do
     case Address.validate(param) do
