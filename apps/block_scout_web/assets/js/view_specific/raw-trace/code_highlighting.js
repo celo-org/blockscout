161246bb01522dbe57159332038f1701d5fb16d3
Valentin Rodygin <valentin@clabs.co>
2021-09-23 15:06:26 +0200

    ES linting

--- apps/block_scout_web/assets/js/view_specific/raw-trace/code_highlighting.js
+++ apps/block_scout_web/assets/js/view_specific/raw-trace/code_highlighting.js
@@ -1,7 +1,7 @@
 import hljs from 'highlight.js/lib/core'
-import json from 'highlight.js/lib/languages/json';
+import json from 'highlight.js/lib/languages/json'
 
-hljs.registerLanguage('json', json);
+hljs.registerLanguage('json', json)
 
 // only activate highlighting on pages with this selector
 if (document.querySelectorAll('[data-activate-highlight]').length > 0) {
