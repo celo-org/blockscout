7185623b0bdd489229b4db95a9e8afd7da91cd10
obasilakis <vasileios.georgopoulos@gmail.com>
2021-11-09 17:30:32 +0100

    Add celo_pending_epoch_operations table, schema and factory

---
+++ apps/explorer/lib/explorer/chain/celo_pending_epoch_operation.ex
@@ -0,0 +1,37 @@
+defmodule Explorer.Chain.CeloPendingEpochOperation do
+  @moduledoc """
+  Tracks epoch blocks that have pending operations.
+  """
+
+  use Explorer.Schema
+
+  alias Explorer.Chain.{Block, Hash}
+
+  @required_attrs ~w(block_hash fetch_epoch_rewards)a
+
+  @typedoc """
+   * `block_hash` - the hash of the epoch block that has pending operations.
+   * `fetch_epoch_rewards` - if the epoch rewards should be fetched (or not)
+  """
+  @type t :: %__MODULE__{
+          block_hash: Hash.Full.t(),
+          fetch_epoch_rewards: boolean()
+        }
+
+  @primary_key false
+  schema "celo_pending_epoch_operations" do
+    field(:fetch_epoch_rewards, :boolean)
+
+    timestamps()
+
+    belongs_to(:block, Block, foreign_key: :block_hash, primary_key: true, references: :hash, type: Hash.Full)
+  end
+
+  def changeset(%__MODULE__{} = pending_ops, attrs) do
+    pending_ops
+    |> cast(attrs, @required_attrs)
+    |> validate_required(@required_attrs)
+    |> foreign_key_constraint(:block_hash)
+    |> unique_constraint(:block_hash, name: :celo_pending_epoch_operations_pkey)
+  end
+end
