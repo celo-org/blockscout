4f3b9ba49dd33db61cb9742c2ee4a3a34bcd4099
obasilakis <vasileios.georgopoulos@gmail.com>
2021-11-17 12:17:50 +0100

    Add missing columns to celo_epoch_rewards

--- apps/explorer/lib/explorer/chain/celo_epoch_rewards.ex
+++ apps/explorer/lib/explorer/chain/celo_epoch_rewards.ex
@@ -22,12 +22,34 @@ defmodule Explorer.Chain.CeloEpochRewards do
           active_votes: Wei.t(),
           total_active_votes: Wei.t(),
           total_reward: Wei.t(),
-          reward: Wei.t()
+          epoch_number: non_neg_integer(),
+          reward: Wei.t(),
+          validator_target_epoch_rewards: Wei.t(),
+          voter_target_epoch_rewards: Wei.t(),
+          community_target_epoch_rewards: Wei.t(),
+          carbon_offsetting_target_epoch_rewards: Wei.t(),
+          target_total_supply: Wei.t(),
+          rewards_multiplier: float(),
+          rewards_multiplier_max: float(),
+          rewards_multiplier_under: float(),
+          rewards_multiplier_over: float(),
+          target_voting_yield: float(),
+          target_voting_yield_max: float(),
+          target_voting_yield_adjustment_factor: float(),
+          target_voting_fraction: float(),
+          voting_fraction: float(),
+          total_locked_gold: Wei.t(),
+          total_non_voting: Wei.t(),
+          total_votes: Wei.t(),
+          electable_validators_max: non_neg_integer(),
+          reserve_gold_balance: Wei.t(),
+          gold_total_supply: Wei.t(),
+          stable_usd_total_supply: Wei.t()
         }
 
-  @attrs ~w( block_hash log_index address_hash active_votes reward total_active_votes total_reward block_number )a
+  @attrs ~w( block_hash log_index address_hash active_votes reward total_active_votes total_reward block_number epoch_number validator_target_epoch_rewards voter_target_epoch_rewards community_target_epoch_rewards carbon_offsetting_target_epoch_rewards target_total_supply rewards_multiplier rewards_multiplier_max rewards_multiplier_under rewards_multiplier_over target_voting_yield target_voting_yield_max target_voting_yield_adjustment_factor target_voting_fraction voting_fraction total_locked_gold total_non_voting total_votes electable_validators_max reserve_gold_balance gold_total_supply stable_usd_total_supply )a
 
-  @required_attrs ~w( block_hash log_index )a
+  @required_attrs ~w( block_hash )a
 
   schema "celo_epoch_rewards" do
     field(:reward, Wei)
@@ -36,6 +58,28 @@ defmodule Explorer.Chain.CeloEpochRewards do
     field(:total_active_votes, Wei)
     field(:log_index, :integer)
     field(:block_number, :integer)
+    field(:epoch_number, :integer)
+    field(:validator_target_epoch_rewards, Wei)
+    field(:voter_target_epoch_rewards, Wei)
+    field(:community_target_epoch_rewards, Wei)
+    field(:carbon_offsetting_target_epoch_rewards, Wei)
+    field(:target_total_supply, Wei)
+    field(:rewards_multiplier, :decimal)
+    field(:rewards_multiplier_max, :decimal)
+    field(:rewards_multiplier_under, :decimal)
+    field(:rewards_multiplier_over, :decimal)
+    field(:target_voting_yield, :decimal)
+    field(:target_voting_yield_max, :decimal)
+    field(:target_voting_yield_adjustment_factor, :decimal)
+    field(:target_voting_fraction, :decimal)
+    field(:voting_fraction, :decimal)
+    field(:total_locked_gold, Wei)
+    field(:total_non_voting, Wei)
+    field(:total_votes, Wei)
+    field(:electable_validators_max, :integer)
+    field(:reserve_gold_balance, Wei)
+    field(:gold_total_supply, Wei)
+    field(:stable_usd_total_supply, Wei)
 
     belongs_to(
       :group_address,
---
+++ apps/explorer/priv/repo/migrations/20211111183251_add_missing_columns_to_celo_epoch_rewards.exs
@@ -0,0 +1,30 @@
+defmodule Explorer.Repo.Migrations.AddMissingColumnsToCeloEpochRewards do
+  use Ecto.Migration
+
+  def change do
+    alter table(:celo_epoch_rewards) do
+      add(:epoch_number, :integer)
+      add(:validator_target_epoch_rewards, :numeric, precision: 100)
+      add(:voter_target_epoch_rewards, :numeric, precision: 100)
+      add(:community_target_epoch_rewards, :numeric, precision: 100)
+      add(:carbon_offsetting_target_epoch_rewards, :numeric, precision: 100)
+      add(:target_total_supply, :numeric, precision: 100)
+      add(:rewards_multiplier, :numeric, precision: 100)
+      add(:rewards_multiplier_max, :numeric, precision: 100)
+      add(:rewards_multiplier_under, :numeric, precision: 100)
+      add(:rewards_multiplier_over, :numeric, precision: 100)
+      add(:target_voting_yield, :numeric, precision: 100)
+      add(:target_voting_yield_max, :numeric, precision: 100)
+      add(:target_voting_yield_adjustment_factor, :numeric, precision: 100)
+      add(:target_voting_fraction, :numeric, precision: 100)
+      add(:voting_fraction, :numeric, precision: 100)
+      add(:total_locked_gold, :numeric, precision: 100)
+      add(:total_non_voting, :numeric, precision: 100)
+      add(:total_votes, :numeric, precision: 100)
+      add(:electable_validators_max, :integer)
+      add(:reserve_gold_balance, :numeric, precision: 100)
+      add(:gold_total_supply, :numeric, precision: 100)
+      add(:stable_usd_total_supply, :numeric, precision: 100)
+    end
+  end
+end
