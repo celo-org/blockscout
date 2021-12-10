c05fc3c59f99dd5962270a6a9f127b0cddd902d9
Donald Hutchison <donald.hutchison@clabs.co>
2021-12-10 15:05:58 +0100

    Remove csv testing iex code.

--- apps/block_scout_web/.iex.exs
+++ apps/block_scout_web/.iex.exs
@@ -4,20 +4,3 @@ import Ecto.Query
 defmodule SQLHelper do
   def to_string(query), do: Ecto.Adapters.SQL.to_sql(:all, Explorer.Repo, query) |> IO.inspect()
 end
-
-defmodule CSVTesting do
-  def get_address() do
-    {:ok, hsh} = Explorer.Chain.Hash.cast(Explorer.Chain.Hash.Address, <<173, 58, 26, 6, 231, 12, 201, 211, 233, 9, 223, 147, 210, 126, 213, 41, 150, 47, 191, 76>> )
-    Repo.one(from a in Explorer.Chain.Address, where: a.hash == ^hsh)
-  end
-
-  def test_export() do
-    addr = get_address()
-    Explorer.Export.CSV.export_transactions(addr, "2021-01-01", "2021-12-06", [])
-  end
-
-  def test_transfers() do
-    addr = get_address()
-    Explorer.Export.CSV.export_token_transfers(addr, "2021-01-01", "2021-12-06", [])
-  end
-end
