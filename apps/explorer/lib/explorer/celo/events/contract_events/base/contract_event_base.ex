82de0f6fdac4c3467de6d843ade94b6664b1dbb6
Donald Hutchison <donald.hutchison@clabs.co>
2022-02-15 19:22:04 +0100

    Generate EpochRewardsDistributedToVotersEvent.

--- apps/explorer/lib/explorer/celo/events/contract_events/base/contract_event_base.ex
+++ apps/explorer/lib/explorer/celo/events/contract_events/base/contract_event_base.ex
@@ -9,8 +9,7 @@ defmodule Explorer.Celo.ContractEvents.Base do
 
       Module.register_attribute __MODULE__, :params, accumulate: true
 
-      alias Explorer.Celo.ContractEvents.EventTransformer
-      alias Explorer.Chain.{CeloContractEvent, Log}
+      alias Explorer.Chain.CeloContractEvent
       import Ecto.Query
 
       @name unquote(name)
@@ -33,12 +32,13 @@ defmodule Explorer.Celo.ContractEvents.Base do
   end
 
   defmacro __before_compile__(_env) do
-    #transformation methods
     #query methods
-    #protocol implementation
 
     quote do
-      common_struct_properties = [
+      alias Explorer.Celo.ContractEvents.EventTransformer
+
+      # define event struct based on event params
+      common_event_properties = [
         :transaction_hash,
         :block_hash,
         :contract_address_hash,
@@ -46,53 +46,57 @@ defmodule Explorer.Celo.ContractEvents.Base do
         name: @name
       ]
 
-      specific_struct_properties = @params |> Enum.map(&(&1.name))
+      specific_event_properties = @params |> Enum.map(&(&1.name))
 
-      defstruct common_struct_properties ++ specific_struct_properties
+      defstruct common_event_properties ++ specific_event_properties
 
-#      defimpl EventTransformer do
-#        import Explorer.Celo.ContractEvents.Common
-#
-#        def from_log(_, %Log{} = log) do
-#          params = log |> Map.from_struct()
-#          from_params(nil, params)
-#        end
-#
-#        def from_params(_, params) do
-#          [value] = decode_data(params.data, [{:uint, 256}])
-#          group = decode_event(params.second_topic, :address)
-#
-#          %unquote(__MODULE__){
-#            transaction_hash: params.transaction_hash,
-#            block_hash: params.block_hash,
-#            contract_address_hash: params.address_hash,
-#            log_index: params.index,
-#            group: group,
-#            value: value
-#          }
-#        end
-#
-#        def from_celo_contract_event(_, %CeloContractEvent{params: params} = contract) do
-#          %{group: group, value: value} = params |> normalise_map()
-#
-#          %unquote(__MODULE__){
-#            transaction_hash: contract.transaction_hash,
-#            block_hash: contract.block_hash,
-#            contract_address_hash: contract.contract_address_hash,
-#            log_index: contract.log_index,
-#            group: group |> ca(),
-#            value: value
-#          }
-#        end
-#
-#        def to_celo_contract_event_params(event) do
-#          event_params = %{params: %{group: event.group |> fa(), value: event.value}}
-#
-#          event
-#          |> extract_common_event_params()
-#          |> Map.merge(event_params)
-#        end
-#      end
+      # Implement EventTransformer protocol to convert between CeloContractEvent, Chain.Log, and this generated type
+      event_module = __MODULE__
+      defimpl EventTransformer do
+        @event event_module
+        import Explorer.Celo.ContractEvents.Common
+        alias Explorer.Chain.{CeloContractEvent, Log}
+
+        def from_log(_, %Log{} = log) do
+          params = log |> Map.from_struct()
+          from_params(nil, params)
+        end
+
+        def from_params(_, params) do
+          [value] = decode_data(params.data, [{:uint, 256}])
+          group = decode_event(params.second_topic, :address)
+
+          %@event{
+            transaction_hash: params.transaction_hash,
+            block_hash: params.block_hash,
+            contract_address_hash: params.address_hash,
+            log_index: params.index,
+            group: group,
+            value: value
+          }
+        end
+
+        def from_celo_contract_event(_, %CeloContractEvent{params: params} = contract) do
+          %{group: group, value: value} = params |> normalise_map()
+
+          %@event{
+            transaction_hash: contract.transaction_hash,
+            block_hash: contract.block_hash,
+            contract_address_hash: contract.contract_address_hash,
+            log_index: contract.log_index,
+            group: group |> ca(),
+            value: value
+          }
+        end
+
+        def to_celo_contract_event_params(event) do
+          event_params = %{params: %{group: event.group |> fa(), value: event.value}}
+
+          event
+          |> extract_common_event_params()
+          |> Map.merge(event_params)
+        end
+      end
     end
   end
 end
