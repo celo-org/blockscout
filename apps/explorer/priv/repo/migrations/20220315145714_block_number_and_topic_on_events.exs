035ba214c7f7d2b09acbfe8ae716464668215873
Donald Hutchison <donald.hutchison@clabs.co>
2022-03-16 14:04:44 +0100

    WIP modifying tests to work with new schema.

--- apps/explorer/priv/repo/migrations/20220315145714_block_number_and_topic_on_events.exs
+++ apps/explorer/priv/repo/migrations/20220315145714_block_number_and_topic_on_events.exs
@@ -1,5 +1,6 @@
 defmodule Explorer.Repo.Migrations.BlockNumberAndTopicOnEvents do
   use Ecto.Migration
+  import Ecto.Query
 
   def up do
     # add new fields
@@ -12,17 +13,17 @@ defmodule Explorer.Repo.Migrations.BlockNumberAndTopicOnEvents do
     flush()
 
     # get block_number and event topic from "parent" log row and update existing event rows
-    from(e in "celo_contract_events",
-    join: l in "logs",
-    on: {l.block_hash, l.index} == {e.block_hash, e.log_index},
-    update: [set: [block_number: l.block_number, topic: l.first_topic]])
-    |> Repo.update_all([])
+    from(event in "celo_contract_events",
+    join: log in "logs",
+    on: {log.block_hash, log.index} == {event.block_hash, event.log_index},
+    update: [set: [block_number: log.block_number, topic: log.first_topic]])
+    |> repo().update_all([])
 
     #use block_number in primary key
     alter table(:celo_contract_events) do
       remove(:block_hash)
-      modify(:block_number, primary_key: true, null: false)
-      modify(:topic, null: false)
+      modify(:block_number, :integer, primary_key: true, null: false)
+      modify(:topic, :string, null: false)
     end
 
     #add indices to block_number and topic
@@ -41,12 +42,12 @@ defmodule Explorer.Repo.Migrations.BlockNumberAndTopicOnEvents do
       join: l in "logs",
       on: {l.block_hash, l.index} == {e.block_hash, e.log_index},
       update: [set: [block_hash: l.block_hash]])
-    |> Repo.update_all([])
+    |> repo().update_all([])
 
     alter table(:celo_contract_events) do
       remove(:block_number)
       remove(:topic)
-      modify(:block_hash, primary_key: true, null: false)
+      modify(:block_hash, :string, primary_key: true, null: false)
     end
   end
 end
