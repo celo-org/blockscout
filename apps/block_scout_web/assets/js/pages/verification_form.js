1402dcae511a1e44d2f193fc4823613d7d7b4d83
Valentin Rodygin <valentin@clabs.co>
2022-03-24 15:42:43 +0100

    Removed unused code

--- apps/block_scout_web/assets/js/pages/verification_form.js
+++ apps/block_scout_web/assets/js/pages/verification_form.js
@@ -129,25 +129,6 @@ const elements = {
 const $contractVerificationPage = $('[data-page="contract-verification"]')
 const $contractVerificationChooseTypePage = $('[data-page="contract-verification-choose-type"]')
 
-function filterNightlyBuilds (filter) {
-  const select = document.getElementById('smart_contract_compiler_version')
-  const options = select.getElementsByTagName('option')
-  for (const option of options) {
-    const txtValue = option.textContent || option.innerText
-    if (filter) {
-      if (txtValue.toLowerCase().indexOf('nightly') > -1) {
-        option.style.display = 'none'
-      } else {
-        option.style.display = ''
-      }
-    } else {
-      if (txtValue.toLowerCase().indexOf('nightly') > -1) {
-        option.style.display = ''
-      }
-    }
-  }
-}
-
 if ($contractVerificationPage.length) {
   const store = createStore(reducer)
   const addressHash = $('#smart_contract_address_hash').val()
@@ -197,6 +178,9 @@ if ($contractVerificationPage.length) {
         uploadMultiple: true,
         addRemoveLinks: true,
         maxFilesize: 20,
+        headers: {
+          "Accept": "*/*"
+        },
         params: { address_hash: $('#smart_contract_address_hash').val() },
         init: function () {
           this.on('addedfile', function (_file) {
@@ -221,57 +205,12 @@ if ($contractVerificationPage.length) {
       document.getElementById('verify-via-json-submit').disabled = (filesLength === 0)
     }
 
-    setTimeout(function () {
-      $('.nightly-builds-false').trigger('click')
-    }, 10)
-
-    $('.js-btn-add-contract-libraries').on('click', function () {
-      $('.js-smart-contract-libraries-wrapper').show()
-      $(this).hide()
-    })
-
-    $('.autodetectfalse').on('click', function () {
-      if ($(this).prop('checked')) { $('.constructor-arguments').show() }
-    })
-
-    $('.autodetecttrue').on('click', function () {
-      if ($(this).prop('checked')) { $('.constructor-arguments').hide() }
-    })
-
-    $('.nightly-builds-true').on('click', function () {
-      if ($(this).prop('checked')) { filterNightlyBuilds(false) }
-    })
-
-    $('.nightly-builds-false').on('click', function () {
-      if ($(this).prop('checked')) { filterNightlyBuilds(true) }
-    })
-
-    $('.optimization-false').on('click', function () {
-      if ($(this).prop('checked')) { $('.optimization-runs').hide() }
-    })
-
-    $('.optimization-true').on('click', function () {
-      if ($(this).prop('checked')) { $('.optimization-runs').show() }
-    })
-
     $('.js-smart-contract-form-reset').on('click', function () {
       $('.js-contract-library-form-group').removeClass('active')
       $('.js-contract-library-form-group').first().addClass('active')
       $('.js-smart-contract-libraries-wrapper').hide()
       $('.js-btn-add-contract-libraries').show()
       $('.js-add-contract-library-wrapper').show()
-    })
-
-    $('.js-btn-add-contract-library').on('click', function () {
-      const nextContractLibrary = $('.js-contract-library-form-group.active').next('.js-contract-library-form-group')
-
-      if (nextContractLibrary) {
-        nextContractLibrary.addClass('active')
-      }
-
-      if ($('.js-contract-library-form-group.active').length === $('.js-contract-library-form-group').length) {
-        $('.js-add-contract-library-wrapper').hide()
-      }
     })
 
     $('#verify-via-json-submit').on('click', function (e) {
@@ -315,14 +254,6 @@ if ($contractVerificationPage.length) {
           onContractUnverified()
         }
       }).fail(onContractUnverified)
-  })
-
-  $('.verify-via-flattened-code').on('click', function () {
-    if ($(this).prop('checked')) {
-      $('#verify_via_flattened_code_button').show()
-      $('#verify_via_sourcify_button').hide()
-      $('#verify_vyper_contract_button').hide()
-    }
   })
 
   $('.verify-via-sourcify').on('click', function () {
