5f5ff002a1e35d1fc6135f4ddc6002ccfb9a30d2
Leszek Stachowski <leszek.stachowski@clabs.co>
2022-03-26 11:06:30 +0100

    161: Fix DB schema parser

--- apps/explorer/lib/explorer/repo/config_helper.ex
+++ apps/explorer/lib/explorer/repo/config_helper.ex
@@ -11,16 +11,16 @@ defmodule Explorer.Repo.ConfigHelper do
   @app_env_vars [
     username: "DATABASE_USER",
     password: "DATABASE_PASSWORD",
-    host: "DATABASE_HOST",
+    hostname: "DATABASE_HOST",
     port: "DATABASE_PORT",
-    database: "DATABASE_DB"
+    database: "DATABASE_NAME"
   ]
 
   # https://hexdocs.pm/postgrex/Postgrex.html#start_link/1-options
   @postgrex_env_vars [
     username: "PGUSER",
     password: "PGPASSWORD",
-    host: "PGHOST",
+    hostname: "PGHOST",
     port: "PGPORT",
     database: "PGDATABASE"
   ]
@@ -29,10 +29,17 @@ defmodule Explorer.Repo.ConfigHelper do
     url = opts[:url] || System.get_env("DATABASE_URL")
     env_function = opts[:env_func] || (&System.get_env/1)
 
-    url
-    |> extract_parameters()
-    |> Keyword.merge(get_env_vars(@postgrex_env_vars, env_function))
-    |> Keyword.merge(get_env_vars(@app_env_vars, env_function))
+    cond do
+      url == "" or is_nil(url) ->
+        [username: "", password: "", hostname: "", port: 5432, database: ""]
+        |> Keyword.merge(get_env_vars(@postgrex_env_vars, env_function))
+        |> Keyword.merge(get_env_vars(@app_env_vars, env_function))
+      true ->
+        url
+        |> extract_parameters()
+        |> Keyword.merge(get_env_vars(@postgrex_env_vars, env_function))
+        |> Keyword.merge(get_env_vars(@app_env_vars, env_function))
+    end
   end
 
   defp extract_parameters(empty) when empty == nil or empty == "", do: []
@@ -54,7 +61,6 @@ defmodule Explorer.Repo.ConfigHelper do
     Enum.reduce(vars, [], fn {name, var}, opts ->
       case env_function.(var) do
         nil -> opts
-        "" -> opts
         env_value -> Keyword.put(opts, name, env_value)
       end
     end)
