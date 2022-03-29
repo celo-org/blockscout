c1970b1122b76fb99ab9c806b0d3174cfb3dcd50
Donald Hutchison <donald.hutchison@clabs.co>
2022-03-29 17:57:44 +0200

    License detection pt1.

--- apps/block_scout_web/lib/block_scout_web/views/verified_contracts_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/verified_contracts_view.ex
@@ -1,4 +1,21 @@
 defmodule BlockScoutWeb.VerifiedContractsView do
   use BlockScoutWeb, :view
 
+  def detect_license(contract) do
+    cond do
+      license = spdx_tag(contract.contract_source_code) ->
+        license
+
+      Explorer.Celo.CoreContracts.is_core_contract_address?(contract.address) ->
+        "Celo"
+
+      true ->
+        "Unknown"
+    end
+  end
+
+  def spdx_tag(source) do
+    ~r/SPDX-License-Identifier:\s+(.*)/
+    |> Regex.run(source, capture: :all_but_first)
+  end
 end
