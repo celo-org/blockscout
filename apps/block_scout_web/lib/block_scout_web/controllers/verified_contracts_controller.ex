a09c83854be54d9168a2d60e469753ca78ec6ade
Donald Hutchison <donald.hutchison@clabs.co>
2022-03-29 15:31:12 +0200

    Listing address.

--- apps/block_scout_web/lib/block_scout_web/controllers/verified_contracts_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/verified_contracts_controller.ex
@@ -1,9 +1,18 @@
 defmodule BlockScoutWeb.VerifiedContractsController do
   use BlockScoutWeb, :controller
 
+  import Ecto.Query
   #alias Explorer.Chain
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    contracts = get_verified_contracts()
+
+    render(conn, "index.html", contracts: contracts)
+  end
+
+  def get_verified_contracts() do
+    Explorer.Chain.SmartContract
+    |> preload(:address)
+    |> Explorer.Repo.all()
   end
 end
