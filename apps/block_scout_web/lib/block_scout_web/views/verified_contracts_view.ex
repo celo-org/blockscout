73e41468b0f567ee51f426f5802272c5ce1022ad
Donald Hutchison <donald.hutchison@clabs.co>
2022-03-30 15:23:22 +0200

    Display CELO balance for contract.

--- apps/block_scout_web/lib/block_scout_web/views/verified_contracts_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/verified_contracts_view.ex
@@ -1,6 +1,10 @@
 defmodule BlockScoutWeb.VerifiedContractsView do
   use BlockScoutWeb, :view
 
+  alias Explorer.Chain.Wei
+  alias Explorer.Chain.SmartContract
+  alias Explorer.Chain.Address
+
   def detect_license(contract) do
     cond do
       license = spdx_tag(contract.contract_source_code) ->
@@ -17,5 +21,15 @@ defmodule BlockScoutWeb.VerifiedContractsView do
   def spdx_tag(source) do
     ~r/SPDX-License-Identifier:\s+(.*)/
     |> Regex.run(source, capture: :all_but_first)
+  end
+
+  def contract_balance(%SmartContract{address: %Address{fetched_coin_balance: balance}}) when not is_nil(balance) do
+    balance
+    |> Wei.to(:ether)
+    |> Decimal.round(3)
+  end
+
+  def contract_balance(_contract) do
+    0
   end
 end
