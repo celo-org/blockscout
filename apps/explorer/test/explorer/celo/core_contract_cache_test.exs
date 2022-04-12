ffbe39b318925948def8164962b7427660e3528e
Donald Hutchison <donald.hutchison@clabs.co>
2022-04-12 16:05:11 +0200

    Test failed contract refresh.

--- apps/explorer/test/explorer/celo/core_contract_cache_test.exs
+++ apps/explorer/test/explorer/celo/core_contract_cache_test.exs
@@ -72,5 +72,39 @@ defmodule Explorer.Celo.CoreContractCacheTest do
       assert CoreContracts.is_core_contract_address?("0x" <> result_address)
       assert CoreContracts.contract_address(test_contract_identifier) == "0x" <> result_address
     end
+
+    test "unsuccessful refresh does not prevent core contract recognition" do
+      test_address = "0xheycoolteststringwheredidyoufindit"
+      test_contract_identifier = "TestID"
+
+      start_supervised(
+        {CoreContracts,
+          %{
+            refresh_period: :timer.hours(1000),
+            cache: %{test_contract_identifier => test_address}
+          }}
+      )
+
+      start_supervised(
+        {Task.Supervisor, name: Explorer.TaskSupervisor},
+        id: Explorer.TaskSupervisor
+      )
+
+
+      EthereumJSONRPC.Mox
+      |> expect(:json_rpc, 2, fn
+        [%{method: "eth_call"}], _options ->
+          raise "oh darn this is a failure"
+      end)
+
+      CoreContracts.refresh()
+
+      # waiting for completion of async blockchain calls
+      :timer.sleep(100)
+
+      #previous address should still be considered a core contract
+      assert CoreContracts.is_core_contract_address?(test_address)
+      assert CoreContracts.contract_address(test_contract_identifier) == test_address
+    end
   end
 end
