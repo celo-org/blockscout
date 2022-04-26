b6284e0b7537d2c8cce4adf66ca48f18f880cc8e
Leszek Stachowski <leszek.stachowski@clabs.co>
2022-04-26 14:31:42 +0200

    Add "if not exists" to the migration

--- apps/explorer/priv/repo/migrations/20220426113555_create_smart_contracts_transaction_counts_view.exs
+++ apps/explorer/priv/repo/migrations/20220426113555_create_smart_contracts_transaction_counts_view.exs
@@ -3,7 +3,7 @@ defmodule Explorer.Repo.Migrations.CreateSmartContractsTransactionCountsView do
 
   def up do
     execute("""
-    create materialized view smart_contract_transaction_counts as
+    create materialized view if not exists smart_contract_transaction_counts as
     with last_block_number as (select max(number) - 17280 * 90 as number from blocks)
     select to_address_hash as address_hash, count(*) as transaction_count from transactions where to_address_hash in (select address_hash from smart_contracts) and block_number > (select number from last_block_number) group by to_address_hash;
     """)
