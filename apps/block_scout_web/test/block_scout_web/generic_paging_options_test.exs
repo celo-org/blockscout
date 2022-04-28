60701c0e2493100c2a68561670f4580a1131a794
Leszek Stachowski <leszek.stachowski@clabs.co>
2022-04-28 13:02:05 +0200

    Add test coverage for generic_paging_options

---
+++ apps/block_scout_web/test/block_scout_web/generic_paging_options_test.exs
@@ -0,0 +1,111 @@
+defmodule BlockScoutWeb.GenericPagingOptionsTest do
+  use Explorer.DataCase
+
+  alias BlockScoutWeb.GenericPagingOptions
+
+  describe "extract_paging_options_from_params/4" do
+    test "provides default values for no order fields" do
+      assert %{
+               order_dir: "desc",
+               order_field: nil,
+               page_number: 1,
+               page_size: 10
+             } == GenericPagingOptions.extract_paging_options_from_params(%{}, 10, [], "desc", 10)
+    end
+
+    test "provides default values for one order field" do
+      assert %{
+               order_dir: "asc",
+               order_field: "count",
+               page_number: 1,
+               page_size: 20
+             } == GenericPagingOptions.extract_paging_options_from_params(%{}, 20, ["count"], "asc", 20)
+    end
+
+    test "provides default value for wrong page number type" do
+      assert %{
+               order_dir: "asc",
+               order_field: "count",
+               page_number: 1,
+               page_size: 20
+             } ==
+               GenericPagingOptions.extract_paging_options_from_params(
+                 %{
+                   "page_number" => "invalid-value",
+                   "page_size" => "invalid-value"
+                 },
+                 20,
+                 ["count"],
+                 "asc",
+                 20
+               )
+    end
+
+    test "provides default value for page number out of range" do
+      assert %{
+               order_dir: "asc",
+               order_field: "count",
+               page_number: 1,
+               page_size: 20
+             } ==
+               GenericPagingOptions.extract_paging_options_from_params(
+                 %{
+                   "page_number" => 4
+                 },
+                 30,
+                 ["count"],
+                 "asc",
+                 10
+               )
+    end
+
+    test "provides default values for multiple order fields" do
+      assert %{
+               order_dir: "asc",
+               order_field: "count",
+               page_number: 1,
+               page_size: 20
+             } == GenericPagingOptions.extract_paging_options_from_params(%{}, 20, ["count", "name"], "asc", 20)
+    end
+
+    test "provides default value when wrong order data provided in params" do
+      assert %{
+               order_dir: "asc",
+               order_field: "count",
+               page_number: 1,
+               page_size: 20
+             } ==
+               GenericPagingOptions.extract_paging_options_from_params(
+                 %{
+                   "order_field" => "invalid-order-field",
+                   "order_dir" => "invalid-order-dir"
+                 },
+                 20,
+                 ["count", "name"],
+                 "asc",
+                 20
+               )
+    end
+
+    test "overrides values when correct data provided in params" do
+      assert %{
+               order_dir: "desc",
+               order_field: "name",
+               page_number: 3,
+               page_size: 5
+             } ==
+               GenericPagingOptions.extract_paging_options_from_params(
+                 %{
+                   "order_field" => "name",
+                   "order_dir" => "desc",
+                   "page_number" => "3",
+                   "page_size" => "5"
+                 },
+                 30,
+                 ["count", "name"],
+                 "asc",
+                 10
+               )
+    end
+  end
+end
