936797fa7d2a76c95224e33b385a39493c824a7d
Donald Hutchison <donald.hutchison@clabs.co>
2022-05-20 16:54:08 +0200

    Implement a basic itx cache.

---
+++ apps/indexer/lib/indexer/celo/internal_transaction_cache.ex
@@ -0,0 +1,79 @@
+defmodule Indexer.Celo.InternalTransactionCache do
+  @moduledoc """
+  Store internal transaction fetch results when blocks are not correctly indexed
+  """
+  use GenServer
+
+  require Logger
+
+  require Explorer.Celo.Telemetry, as: Telemetry
+
+  @cache_time :timer.minutes(10)
+
+  def store(block_number, itx) do
+    pid = Process.whereis(__MODULE__)
+
+    if pid do GenServer.cast(pid, {:store, block_number, itx} ) end
+  end
+
+
+  def get(block_number) do
+    pid = Process.whereis(__MODULE__)
+
+    if pid do GenServer.call(pid, {:get, block_number} ) end
+  end
+
+  def clear(block_number) do
+    pid = Process.whereis(__MODULE__)
+
+    if pid do GenServer.cast(pid, {:clear, block_number} ) end
+  end
+
+  def start_link([init_opts, gen_server_opts]) do
+    start_link(init_opts, gen_server_opts)
+  end
+
+  def start_link(init_opts, gen_server_opts) do
+    GenServer.start_link(__MODULE__, init_opts, gen_server_opts)
+  end
+
+  def init(opts) do
+    {:ok,
+      %{
+        cache: %{},
+        timers: %{},
+      }
+    }
+  end
+
+  def handle_cast({:store, block_number, itx}, _from, state) do
+    state = state
+            |> put_in([:cache, block_number], itx)
+            |> put_in([:timers, block_number], Process.send_after(self(), {:clear, block_number}, @cache_time))
+
+    {:noreply, state}
+  end
+
+  def handle_call({:get, block_number}, _from, %{cache: cache} = state) do
+    itx = cache[block_number]
+    {:reply, itx, state}
+  end
+
+  defp remove_item(block_number, state = %{cache: cache, timers: timers}) do
+    cache = Map.delete(cache,block_number)
+
+    timer = timers[block_number]
+    Process.cancel_timer(timer)
+    timers = Map.delete(timers, block_number)
+
+    %{cache: cache, timers: timers}
+  end
+
+  def handle_cast({:clear, block_number}, _from, state) do
+    {:noreply, remove_item(block_number, state)}
+  end
+
+  def handle_info({:clear, block_number}, _from, state) do
+    {:noreply, remove_item(block_number, state)}
+  end
+end
