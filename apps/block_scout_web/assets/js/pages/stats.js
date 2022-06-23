d8ab8b2bf162fc21d2df0595420871be680223cc
enrique <enrique@keyko.io>
2022-06-23 15:54:34 +0200

    Keep padding

--- apps/block_scout_web/assets/js/pages/stats.js
+++ apps/block_scout_web/assets/js/pages/stats.js
@@ -8,10 +8,8 @@ $('.stats-link').on('click', function () {
 $(window).on('load resize', function () {
   var width = $(window).width()
   if (width < 768) {
-    $('.js-ad-dependant-pt').removeClass('pt-5')
     $('.menu-wrap').removeClass('container')
   } else {
-    $('.js-ad-dependant-pt').addClass('pt-5')
     $('.menu-wrap').addClass('container')
   }
 })
