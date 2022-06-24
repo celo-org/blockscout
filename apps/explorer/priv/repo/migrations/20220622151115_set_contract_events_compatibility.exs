14d23467d07a90fd4346636987468ee7350c9606
Donald Hutchison <donald.hutchison@clabs.co>
2022-06-24 16:19:19 +0200

    Don't change primary key, just add big query replication id.

--- apps/explorer/priv/repo/migrations/20220622151115_set_contract_events_compatibility.exs
+++ apps/explorer/priv/repo/migrations/20220622151115_set_contract_events_compatibility.exs
@@ -3,23 +3,8 @@ defmodule Explorer.Repo.Migrations.SetContractEventsCompatibility do
 
   def change do
     execute(
-      "ALTER TABLE celo_contract_events ADD COLUMN id bigserial",
-      "ALTER TABLE celo_contract_events DROP COLUMN id"
-    )
-
-    execute(
-      "ALTER TABLE celo_contract_events DROP CONSTRAINT celo_contract_events_pkey",
-      "ALTER TABLE celo_contract_events ADD PRIMARY KEY (block_number, log_index)"
-    )
-
-    execute(
-      "ALTER TABLE celo_contract_events ADD PRIMARY KEY (id)",
-      "ALTER TABLE celo_contract_events DROP CONSTRAINT celo_contract_events_pkey"
-    )
-
-    execute(
-      "CREATE UNIQUE INDEX celo_contract_events_unique_block_number_log_index on celo_contract_events(block_number, log_index)",
-      "DROP INDEX IF EXISTS celo_contract_events_unique_block_number_log_index"
+      "ALTER TABLE celo_contract_events ADD COLUMN bq_rep_id bigserial",
+      "ALTER TABLE celo_contract_events DROP COLUMN bq_rep_id"
     )
   end
 end
