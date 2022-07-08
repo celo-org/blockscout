2d1c6930683d462f981e68aa9ae947199972a3e5
Donald Hutchison <donald.hutchison@clabs.co>
2022-07-08 13:05:49 +0200

    Add migration for tracked events.

---
+++ apps/explorer/priv/repo/migrations/20220707153726_tracked_events_schema.exs
@@ -0,0 +1,40 @@
+defmodule Explorer.Repo.Migrations.TrackedEventsSchema do
+  use Ecto.Migration
+
+  def change do
+    create table(:clabs_contract_event_trackings) do
+      add(:smart_contract_id, references(:smart_contracts), null: false)
+      add(:transaction_hash, references(:transactions, column: "hash", type: "bytea"), null: false)
+
+      add(:abi, :jsonb, null: false)
+      add(:topic, :string, null: false)
+      add(:name, :string, null: false)
+
+      add(:backfilled, :boolean, null: false, default: false)
+
+      timestamps()
+    end
+
+    create(index(:clabs_contract_event_trackings, :topic))
+    create(index(:clabs_contract_event_trackings, :backfilled))
+
+    create table(:clabs_tracked_contract_events, primary_key: false) do
+      add(:block_number, :integer, primary_key: true, null: false)
+      add(:log_index, :integer, primary_key: true, null: false)
+
+      add(:contract_event_tracking_id, references(:clabs_contract_event_tracking), null: false)
+      add(:contract_address_hash, references(:smart_contracts, :address_hash, type: :bytea), null: false)
+
+      add(:topic, :string, null: false)
+      add(:name, :string, null: false)
+      add(:params, :jsonb)
+
+      add(:bq_rep_id, :bigserial)
+
+      timestamps()
+    end
+
+    create(index(:clabs_tracked_contract_events, :block_number))
+    create(index(:clabs_tracked_contract_events, :updated_at))
+  end
+end
