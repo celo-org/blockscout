8bc5523517c88525421bb5eaaae7c437bfb5d1ed
Donald Hutchison <donald.hutchison@clabs.co>
2022-07-11 16:08:11 +0200

    Fix migration errors.

--- apps/explorer/lib/explorer/chain/celo/contract_event_tracking.ex
+++ apps/explorer/lib/explorer/chain/celo/contract_event_tracking.ex
@@ -5,14 +5,35 @@ defmodule Explorer.Chain.Celo.ContractEventTracking do
   require Logger
 
   alias __MODULE__
+  alias Explorer.Chain.Hash
+  alias Explorer.Chain.Hash.Address
   alias Explorer.Repo
+
   use Explorer.Schema
   import Ecto.Query
 
   @type t :: %__MODULE__{
-               stat_type: String.t(),
-               value: non_neg_integer()
+               abi: map(),
+               name: String.t(),
+               topic: String.t(),
+               backfilled: boolean(),
+               enabled: boolean(),
+               transaction_hash: Hash.Full.t(),
+               smart_contract_id: non_neg_integer()
              }
+
   schema "clabs_contract_event_trackings" do
+    field(:abi, :map)
+    field(:name, :string)
+    field(:topic, :string)
+    field(:backfilled, :boolean)
+    field(:enabled, :boolean)
+
+    belongs_to(:smart_contract, SmartContract)
+    belongs_to(:transaction, Transactions, foreign_key: :transaction_hash, references: :hash, type: Hash.Address)
+
+    has_one :address, through: [:smart_contract, :address_hash]
+
+    timestamps(null: false, type: :utc_datetime_usec)
   end
 end
--- apps/explorer/priv/repo/migrations/20220707153726_tracked_events_schema.exs
+++ apps/explorer/priv/repo/migrations/20220707153726_tracked_events_schema.exs
@@ -4,14 +4,14 @@ defmodule Explorer.Repo.Migrations.TrackedEventsSchema do
   def change do
     create table(:clabs_contract_event_trackings) do
       add(:smart_contract_id, references(:smart_contracts), null: false)
-      add(:transaction_hash, references(:transactions, column: "hash", type: "bytea"), null: false)
+      add(:transaction_hash, references(:transactions, column: :hash, type: :bytea), null: false)
 
       add(:abi, :jsonb, null: false)
       add(:topic, :string, null: false)
       add(:name, :string, null: false)
 
       add(:backfilled, :boolean, null: false, default: false)
-      add(:enabled, :boolean, null: false, default: false)
+      add(:enabled, :boolean, null: false, default: true)
 
       timestamps()
     end
@@ -23,8 +23,8 @@ defmodule Explorer.Repo.Migrations.TrackedEventsSchema do
       add(:block_number, :integer, primary_key: true, null: false)
       add(:log_index, :integer, primary_key: true, null: false)
 
-      add(:contract_event_tracking_id, references(:clabs_contract_event_tracking), null: false)
-      add(:contract_address_hash, references(:smart_contracts, :address_hash, type: :bytea), null: false)
+      add(:contract_event_tracking_id, references(:clabs_contract_event_trackings), null: false)
+      add(:contract_address_hash, references(:smart_contracts, column: :address_hash, type: :bytea), null: false)
 
       add(:topic, :string, null: false)
       add(:name, :string, null: false)
@@ -37,8 +37,5 @@ defmodule Explorer.Repo.Migrations.TrackedEventsSchema do
 
     create(index(:clabs_tracked_contract_events, :block_number))
     create(index(:clabs_tracked_contract_events, :updated_at))
-    # Intentionally not adding a GIN index to params as in celo_contract_events as generic contracts aren't
-    # integral to the celo protocol. When necessary partial indices over contract addresses can be added for the same
-    # result.
   end
 end
