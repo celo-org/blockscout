7925cf38717311aba741e000c04eefce2eeee445
Leszek Stachowski <leszek.stachowski@clabs.co>
2022-08-03 10:12:54 +0200

    CR fix

--- apps/indexer/lib/indexer/transform/token_transfers.ex
+++ apps/indexer/lib/indexer/transform/token_transfers.ex
@@ -82,7 +82,7 @@ defmodule Indexer.Transform.TokenTransfers do
 
   @doc "Discerns CELO token transfers from internal transactions"
   def parse_itx(txs, gold_token) do
-    initial_acc = %{token_transfers: [], gold_token: gold_token, transfer_count: 1}
+    initial_acc = %{token_transfers: [], gold_token: gold_token, transfer_count: 0}
 
     txs
     |> Enum.filter(fn a -> a.value > 0 end)
@@ -101,7 +101,9 @@ defmodule Indexer.Transform.TokenTransfers do
       block_hash: itx.block_hash,
       # Celo token transfers discerned from itx do not have a valid log index, so an id is calculated here to
       # satisfy schema constraints
-      log_index: -transfer_count,
+      # and it needs to be negative (starting with -1) not to generate conflicts with
+      # log_index for token transfers from block transactions as they start from 0
+      log_index: -1 * (transfer_count + 1),
       from_address_hash: itx.from_address_hash,
       to_address_hash: to_hash,
       token_contract_address_hash: gold_token,
