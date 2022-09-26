26cd98f307ec0d2d049c3c959ff0e0ce9d201dc9
Leszek Stachowski <leszek.stachowski@clabs.co>
2022-09-26 11:28:16 +0200

    eslint

--- apps/block_scout_web/assets/js/lib/campaign_banner.js
+++ apps/block_scout_web/assets/js/lib/campaign_banner.js
@@ -5,48 +5,48 @@ const CLOSED_CAMPAIGN_IDS_KEY = 'closed-campaign-ids'
 
 const getClosedCampaignIdsFromLocalStorage = () => {
   if (window.localStorage) {
-    const closedCampaignIdsFromLocalStorage = window.localStorage.getItem(CLOSED_CAMPAIGN_IDS_KEY);
+    const closedCampaignIdsFromLocalStorage = window.localStorage.getItem(CLOSED_CAMPAIGN_IDS_KEY)
 
     if (typeof closedCampaignIdsFromLocalStorage === 'string') {
       try {
-        const closedCampaignIds = JSON.parse(closedCampaignIdsFromLocalStorage);
+        const closedCampaignIds = JSON.parse(closedCampaignIdsFromLocalStorage)
 
         if (Array.isArray(closedCampaignIds)) {
-          return closedCampaignIds;
+          return closedCampaignIds
         }
       } catch (err) {
-        return [];
+        return []
       }
     }
   }
 
-  return [];
+  return []
 }
 
 const storeClosedCampaigns = (campaignIds) => {
   if (window.localStorage) {
-    window.localStorage.setItem(CLOSED_CAMPAIGN_IDS_KEY, JSON.stringify(campaignIds));
+    window.localStorage.setItem(CLOSED_CAMPAIGN_IDS_KEY, JSON.stringify(campaignIds))
   }
 }
 
 $('.campaign-banner').each((_, banner) => {
-  const $banner = $(banner);
-  const campaignId = $banner.data('campaign-id');
-  const closedCampaignIds = getClosedCampaignIdsFromLocalStorage();
+  const $banner = $(banner)
+  const campaignId = $banner.data('campaign-id')
+  const closedCampaignIds = getClosedCampaignIdsFromLocalStorage()
 
   if (!closedCampaignIds.includes(campaignId)) {
-    $banner.removeClass('campaign-banner-closed');
+    $banner.removeClass('campaign-banner-closed')
     $banner.find('.campaign-banner-close').on('click', () => {
       if (!closedCampaignIds.includes(campaignId)) {
-        closedCampaignIds.push(campaignId);
+        closedCampaignIds.push(campaignId)
       }
-  
-      storeClosedCampaigns(closedCampaignIds);
-      $banner.addClass('campaign-banner-closing');
-  
+
+      storeClosedCampaigns(closedCampaignIds)
+      $banner.addClass('campaign-banner-closing')
+
       setTimeout(() => {
-        $banner.addClass('campaign-banner-closed');
-      }, 1000);
-    });
+        $banner.addClass('campaign-banner-closed')
+      }, 1000)
+    })
   }
-});
+})
