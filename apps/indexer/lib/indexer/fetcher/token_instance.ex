62d4252622230aeb984a1c97a721c30b73e30936
Leszek Stachowski <leszek.stachowski@clabs.co>
2022-10-17 14:53:49 +0200

    Set batch size to 1

--- apps/indexer/lib/indexer/fetcher/token_instance.ex
+++ apps/indexer/lib/indexer/fetcher/token_instance.ex
@@ -17,7 +17,7 @@ defmodule Indexer.Fetcher.TokenInstance do
 
   @defaults [
     flush_interval: 300,
-    max_batch_size: 5,
+    max_batch_size: 1,
     max_concurrency: 100,
     task_supervisor: Indexer.Fetcher.TokenInstance.TaskSupervisor
   ]
