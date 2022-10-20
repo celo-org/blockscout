fe6c2ddc4cb7bccf992a9f8147f65bb6e6aa1adf
Donald Hutchison <donald.hutchison@clabs.co>
2022-10-20 13:59:42 +0200

    Credo.

--- apps/indexer/lib/indexer/block/fetcher.ex
+++ apps/indexer/lib/indexer/block/fetcher.ex
@@ -308,7 +308,7 @@ defmodule Indexer.Block.Fetcher do
          # extract address token balances from token transfers
          address_token_balances = AddressTokenBalances.params_set(%{token_transfers_params: token_transfers}),
 
-         # TODO 20/10/22 : Assert that celo token balances are still inserted correctly and remove the referenced code below
+         # 20/10/22 : Assert that celo token balances are still inserted correctly and remove the referenced code below
          # address_token_balances = add_celo_token_balances(celo_token, addresses, address_token_balances_from_transfers),
 
          {:ok, inserted} <-
