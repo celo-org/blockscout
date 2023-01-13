d1f2d1eb649732a08d61fb8e15b8a5d7ae9ef92b
Donald Hutchison <donald.hutchison@clabs.co>
2023-01-13 15:11:30 +0100

    Extracted block instrumenter.

--- apps/indexer/lib/indexer/celo/telemetry/instrumentation/indexer.ex
+++ apps/indexer/lib/indexer/celo/telemetry/instrumentation/indexer.ex
@@ -10,7 +10,17 @@ defmodule Indexer.Celo.Telemetry.Instrumentation do
         measurement: :count,
         description: "Blockchain primitives ingested via `Import.all` by type",
         tags: [:type]
-      )
+      ),
+      last_value(
+        "indexer_blocks_pending_blockcount_current",
+        event_name: [:indexer, :blocks, :pending_blockcount],
+        measurement: :value,
+        description: "Number of rows in pending_block_operations table / blocks that still need itx fetched"),
+      last_value(
+        "indexer_blocks_pending_current",
+        event_name: [:indexer, :blocks, :pending],
+        measurement: :value,
+        description: "Number of blocks still to be fetched in past range"),
     ]
   end
 end
