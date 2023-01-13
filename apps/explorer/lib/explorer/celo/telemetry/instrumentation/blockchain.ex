fb7d45cf2064c698503de380980b7723b997f648
Donald Hutchison <donald.hutchison@clabs.co>
2023-01-13 13:58:26 +0100

    wip block instrumenter.

--- apps/explorer/lib/explorer/celo/telemetry/instrumentation/blockchain.ex
+++ apps/explorer/lib/explorer/celo/telemetry/instrumentation/blockchain.ex
@@ -2,13 +2,25 @@ defmodule Explorer.Celo.Telemetry.Instrumentation.Blockchain do
   alias Explorer.Celo.Telemetry.Instrumentation
   use Instrumentation
 
+
   def metrics() do
+    # metric names reference `indexer` + appended with `_current` for backwards compatibility
   [
     last_value(
       "indexer_blocks_last_block_number_current",
       event_name: [:indexer, :blocks, :last_block_number],
       measurement: :value,
-      description: "Max block number found in DB")
+      description: "Max block number found in DB"),
+    last_value(
+      "indexer_blocks_last_block_age_current",
+      event_name: [:indexer, :blocks, :last_block_age],
+      measurement: :value,
+      description: "Difference between max block timestamp and current DB time"),
+    last_value(
+      "indexer_blocks_average_time_current",
+      event_name: [:indexer, :blocks, :average_time],
+      measurement: :value,
+      description: "Average block time over past 100 blocks"),
   ]
   end
 end
