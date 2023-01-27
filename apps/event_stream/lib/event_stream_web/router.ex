264ebd581e67ba02acdd423e5166368fb9976c13
Donald Hutchison <donald.hutchison@clabs.co>
2023-01-27 17:20:30 +0100

    Set dashboard for production.

--- apps/event_stream/lib/event_stream_web/router.ex
+++ apps/event_stream/lib/event_stream_web/router.ex
@@ -32,12 +32,10 @@ defmodule EventStream.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
       pipe_through :browser
       live_dashboard "/dashboard", metrics: EventStream.Metrics
     end
-  end
 end
