a5bf014b8d53278e4209b3018048622c5fc86336
Leszek Stachowski <leszek.stachowski@clabs.co>
2023-02-07 10:20:20 +0100

    change resolution to operation

--- apps/explorer/lib/explorer/celo/telemetry/instrumentation/api.ex
+++ apps/explorer/lib/explorer/celo/telemetry/instrumentation/api.ex
@@ -6,33 +6,31 @@ defmodule Explorer.Celo.Telemetry.Instrumentation.Api do
 
   def metrics do
     [
-      distribution("api_graphql_resolution_duration_milliseconds",
+      distribution("api_graphql_operation_duration_milliseconds",
         reporter_options: [
           buckets: [
-            30,
             50,
             100,
-            200,
             300,
             500,
             :timer.seconds(1),
-            :timer.seconds(2),
             :timer.seconds(3),
             :timer.seconds(5),
             :timer.seconds(10),
-            :timer.seconds(20),
             :timer.seconds(30),
             :timer.minutes(1)
           ]
         ],
-        event_name: [:absinthe, :resolve, :field, :stop],
+        event_name: [:absinthe, :execute, :operation, :stop],
         measurement: :duration,
-        description: "Resolution times for requests send to Blockscout API",
-        tags: [:path],
+        description: "Operation times for requests send to Blockscout API",
+        tags: [:name],
         tag_values: fn metadata ->
-          path = Absinthe.Resolution.path(metadata.resolution)
-
-          Map.put(%{}, :path, path)
+          if Keyword.has_key?(metadata.options, :operation_name) and is_binary(metadata.options[:operation_name]) do
+            Map.put(%{}, :name, metadata.options[:operation_name])
+          else
+            %{}
+          end
         end,
         unit: {:native, :millisecond}
       )
