5fc47b98e0a41bbb0d8724723740d40ad9aa4452
Donald Hutchison <donald.hutchison@clabs.co>
2023-02-20 12:21:30 +0100

    Fix warning

--- apps/event_stream/lib/event_stream_web/live/received_live.ex
+++ apps/event_stream/lib/event_stream_web/live/received_live.ex
@@ -24,21 +24,4 @@ defmodule EventStream.ReceivedLive do
   end
 
 
-  defp generate_event(id) do
-    %Explorer.Chain.CeloContractEvent{
-      block_number: 17777818,
-      contract_address_hash: "0x471ece3750da237f93b8e339c536989b8978a438",
-      inserted_at: ~U[2023-02-16 14:19:20.260051Z],
-      log_index: id,
-      name: "Transfer",
-      params: %{
-        "from" => "\\xb460f9ae1fea4f77107146c1960bb1c978118816",
-        "to" => "\\x0ef38e213223805ec1810eebd42153a072a2d89a",
-        "value" => 6177463272192542
-      },
-      topic: "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-      transaction_hash: "0x7640d07bdc3b51065169b8f6a4720c2f807716f31e40212d81b41dfe1441668b",
-      updated_at: ~U[2023-02-16 14:19:20.260051Z]
-    }
-  end
 end
