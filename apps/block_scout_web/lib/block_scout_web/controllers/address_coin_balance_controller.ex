1f8e23eb697cf41553272868bc3c002ba32465c6
Leszek Stachowski <leszek.stachowski@clabs.co>
2023-03-13 16:15:58 +0100

    missed refactoring

--- apps/block_scout_web/lib/block_scout_web/controllers/address_coin_balance_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/address_coin_balance_controller.ex
@@ -99,9 +99,6 @@ defmodule BlockScoutWeb.AddressCoinBalanceController do
 
         case Chain.Hash.Address.validate(address_hash_string) do
           {:ok, _} ->
-            {validator_or_group_sum, voting_sum, locked_gold, vote_activated_gold, pending_gold} =
-              EpochUtil.get_address_summary(address)
-
             render(
               conn,
               "index.html",
@@ -111,11 +108,7 @@ defmodule BlockScoutWeb.AddressCoinBalanceController do
               counters_path: address_path(conn, :address_counters, %{"id" => Address.checksum(address_hash)}),
               current_path: Controller.current_full_path(conn),
               tags: get_address_tags(address_hash, current_user(conn)),
-              validator_or_group_sum: validator_or_group_sum,
-              voting_sum: voting_sum,
-              locked_gold: locked_gold,
-              vote_activated_gold: vote_activated_gold,
-              pending_gold: pending_gold
+              celo_epoch: EpochUtil.get_address_summary(address)
             )
 
           _ ->
