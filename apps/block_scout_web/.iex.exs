6cd2ba6467f624dbc3595c37bd16cf444531c94d
Donald Hutchison <donald.hutchison@clabs.co>
2023-06-02 17:18:28 +0200

    Configure explorer processes that write to db + test function for slow query.

--- apps/block_scout_web/.iex.exs
+++ apps/block_scout_web/.iex.exs
@@ -4,3 +4,10 @@ import Ecto.Query
 defmodule SQLHelper do
   def to_string(query), do: Ecto.Adapters.SQL.to_sql(:all, Explorer.Repo, query) |> IO.inspect()
 end
+
+defmodule Clabs.Debug do
+  def test_bad_graphql do
+    {:ok,hsh} = Explorer.Chain.Hash.Address.cast("0x6131a6d616a4be3737b38988847270a64bc10caa")
+    Explorer.GraphQL.token_txtransfers_query_for_address(hsh)
+  end
+end
