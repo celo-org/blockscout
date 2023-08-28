1282a102d540bfed96c5204cbf78d04fdc4ebf0c
Donald Hutchison <donald.hutchison@clabs.co>
2023-08-28 10:06:02 +0200

    Remove withdrawals tabel from migration (we dont' have it)

--- apps/explorer/priv/repo/migrations/20230821120625_drop_rest_address_foreign_keys.exs
+++ apps/explorer/priv/repo/migrations/20230821120625_drop_rest_address_foreign_keys.exs
@@ -8,7 +8,6 @@ defmodule Explorer.Repo.Migrations.DropRestAddressForeignKeys do
     drop_if_exists(constraint(:block_rewards, :block_rewards_address_hash_fkey))
     drop_if_exists(constraint(:decompiled_smart_contracts, :decompiled_smart_contracts_address_hash_fkey))
     drop_if_exists(constraint(:smart_contracts, :smart_contracts_address_hash_fkey))
-    drop_if_exists(constraint(:withdrawals, :withdrawals_address_hash_fkey))
     drop_if_exists(constraint(:blocks, :blocks_miner_hash_fkey))
   end
 end
