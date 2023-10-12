5cdc2c85ee995edfff97c71373461d41c7a04215
Donald Hutchison <donald.hutchison@clabs.co>
2023-10-12 17:31:14 +0200

    Handling addresses in javascript.

--- apps/explorer/lib/explorer/celo/sanction_cache.ex
+++ apps/explorer/lib/explorer/celo/sanction_cache.ex
@@ -17,7 +17,7 @@ defmodule Explorer.Celo.SanctionCache do
 
   @impl true
   def handle_continue(:update_with_public_list, state) do
-    new_state = Map.put(state, :list, ["0xtestaddresspleaseblockme"])
+    new_state = Map.put(state, :list, ["0x0143008E904fEea7140c831585025bc174eB2F15", "0xtestaddresspleaseblockme"])
     {:noreply, new_state}
   end
 
