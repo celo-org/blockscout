868da57974bfcf540b736ee4a3b3b6c777d649bd
Donald Hutchison <donald.hutchison@clabs.co>
2023-10-16 12:04:01 +0200

    Fetching sanction list on app startup.

--- apps/explorer/lib/explorer/celo/sanction_cache.ex
+++ apps/explorer/lib/explorer/celo/sanction_cache.ex
@@ -2,6 +2,7 @@ defmodule Explorer.Celo.SanctionCache do
   @moduledoc "A cache to manage addresses blocked for interaction"
 
   use GenServer
+  require Logger
 
   @spec start_link(term()) :: GenServer.on_start()
   def start_link(params \\ %{}) do
@@ -10,15 +11,37 @@ defmodule Explorer.Celo.SanctionCache do
 
   @impl true
   def init(params) do
-    state = Map.get(params, :initial_state, %{})
+    state = Map.get(params, :initial_state, %{list: []})
 
     {:ok, state, {:continue, :update_with_public_list}}
   end
 
   @impl true
   def handle_continue(:update_with_public_list, state) do
-    new_state = Map.put(state, :list, ["0x0143008E904fEea7140c831585025bc174eB2F15", "0xtestaddresspleaseblockme"])
-    {:noreply, new_state}
+    sanctions_url = :explorer
+      |> Application.get_env(:celo_sanctions)
+      |> Keyword.get(:url, "https://celo-org.github.io/compliance/ofac.sanctions.json")
+
+    Logger.info("Fetching sanctioned address list from #{sanctions_url}")
+
+    {:ok, response} = case HTTPoison.get(sanctions_url, [], follow_redirect: true, timeout: 5_000) do
+      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
+        {:ok, body}
+
+      _ ->
+        {:error, :error_calling_backend_api}
+    end
+
+    sanction_list = response |> Jason.decode!()
+
+    Logger.info("Retrieved #{length(sanction_list)} sanctioned addresses (md5 #{hash_list(sanction_list)}")
+
+    {:noreply, %{state | list: sanction_list}}
+  end
+
+  @doc "printable md5 hash of list contents to assert changed values"
+  def hash_list(list) do
+    :crypto.hash(:md5, :erlang.term_to_binary(list)) |> Base.encode16()
   end
 
   @impl true
